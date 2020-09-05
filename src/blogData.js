d3c8951f7e08ca369dff16c495368d32a879be57
kh45 <khaledhassan45@gmail.com>
2020-09-05 11:32:06 -0400

    adding content to blog section

---
+++ src/blogData.js
@@ -0,0 +1,18 @@
+export const blogList = [
+    {title: 'How to Scrape Dynamic Content Off of a Web Page Using Rails, Nokogiri, and Watir',
+    preview: 'As a web developer, you may one day find yourself in the predicament of having to scrape data off of a website. This is the situation I found myself in when working on my final project for The Flatiron...',
+    img: './assets/blogs/Watir_logo.png'},
+    {title: 'How to Scrape Dynamic Content Off of a Web Page Using Rails, Nokogiri, and Watir',
+    preview: 'As a web developer, you may one day find yourself in the predicament of having to scrape data off of a website. This is the situation I found myself in when working on my final project for The Flatiron...',
+    img: './assets/blogs/Watir.png'},
+    {title: 'How to Scrape Dynamic Content Off of a Web Page Using Rails, Nokogiri, and Watir',
+    preview: 'As a web developer, you may one day find yourself in the predicament of having to scrape data off of a website. This is the situation I found myself in when working on my final project for The Flatiron...',
+    img: './assets/blogs/Watir_logo.png'},
+    {title: 'How to Scrape Dynamic Content Off of a Web Page Using Rails, Nokogiri, and Watir',
+    preview: 'As a web developer, you may one day find yourself in the predicament of having to scrape data off of a website. This is the situation I found myself in when working on my final project for The Flatiron...',
+    img: './assets/blogs/Watir_logo.png'},
+    {title: 'How to Scrape Dynamic Content Off of a Web Page Using Rails, Nokogiri, and Watir',
+    preview: 'As a web developer, you may one day find yourself in the predicament of having to scrape data off of a website. This is the situation I found myself in when working on my final project for The Flatiron...',
+    img: './assets/blogs/Watir_logo.png'}
+]
+    
