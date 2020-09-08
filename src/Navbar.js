94cbda5898650c90a501ff03b60ccb21650e28b7
kh45 <khaledhassan45@gmail.com>
2020-09-08 11:43:55 -0400

    implemented bootstrap navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,54 +9,56 @@ class Navbarz extends React.Component {
         window.open("https://drive.google.com/file/d/1rGVrVuMQUlFjLcqpTfCzbJ5PkIH3pP4C/view")
     }
 
-    responsiveBars() {
-
+    makeResponsive(event) {
+        event.target.parentNode.parentNode.classList.add('responsive')
     }
 
     render() {
         return (
-//             <Navbar bg="light" expand="lg" className="navz sticky-top">
-//                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-//                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//                 <Navbar.Collapse id="basic-navbar-nav">
-//                 <Nav className="mr-auto">
-//       {/* <Nav.Link as='div'><Link activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Section 1</Link></Nav.Link> */}
-//       <Nav.Link as="div"><Link className="nav-item" activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Section 1</Link></Nav.Link>
-//       <Link className="nav-item" activeClass="active" to="section2" spy={true} smooth={true} offset={-70} duration={500}>Section 2</Link>
-//       <Link className="nav-item" activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Section 1</Link>
-//       <Nav.Link href="#link">Link</Nav.Link>
-//       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-//         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-//         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-//         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-//         <NavDropdown.Divider />
-//         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-//       </NavDropdown>
-//     </Nav>
-//     {/* <Form inline> */}
-//       <Button variant="outline-success">Search</Button>
-//     {/* </Form> */}
-//   </Navbar.Collapse>
-// </Navbar>
-            <nav className="nav" id="nav">
-                <div className="nav-content">
-                    <ul className="nav-items">
-                        {/* <span className="first-nav"> */}
-                        <li className="nav-item"><Link activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Home</Link></li>
-                        <li className="nav-item"><Link activeClass="active" to="section2" spy={true} smooth={true} duration={500}>About</Link></li>
-                        <li className="nav-item"><Link activeClass="active" to="section3" spy={true} smooth={true} duration={500}>Portfolio</Link></li>
-                        <li className="nav-item"><Link activeClass="active" to="section4" spy={true} smooth={true} duration={500}>Blog</Link></li>
-                        <li className="nav-item"><Link activeClass="active" to="section5" spy={true} smooth={true} duration={500}>Contact</Link></li>
-                        {/* </span> */}
-                        {/* <span className="resume"> */}
-                            <li className="nav-item resume" onClick={this.handleClick}>Résumé</li>
-                            {/* </span> */}
-                        <li className="nav-item bars">
-                            <i class="fas fa-bars fa-3x" onClick={this.responsiveBars}></i>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <Navbar bg="light" expand="lg" className="navz sticky-top">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+      {/* <Nav.Link as='div'><Link activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Section 1</Link></Nav.Link> */}
+      <Link className="nav-item" activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Home</Link>
+      <Link className="nav-item" activeClass="active" to="section2" spy={true} smooth={true} offset={-70} duration={500}>About</Link>
+      <Link className="nav-item" activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Portfolio</Link>
+      <Link className="nav-item" activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Blog</Link>
+      <Link className="nav-item" activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Contact</Link>
+      
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    {/* <Form inline> */}
+      <Button variant="outline-success">Search</Button>
+    {/* </Form> */}
+  </Navbar.Collapse>
+</Navbar>
+            // <nav className="nav" id="nav">
+            //     <div className="nav-content">
+            //         <ul className="nav-items">
+            //             {/* <span className="first-nav"> */}
+            //             <li className="nav-item"><Link activeClass="active" to="section1" spy={true} smooth={true} offset={-70} duration={500}>Home</Link></li>
+            //             <li className="nav-item"><Link activeClass="active" to="section2" spy={true} smooth={true} duration={500}>About</Link></li>
+            //             <li className="nav-item"><Link activeClass="active" to="section3" spy={true} smooth={true} duration={500}>Portfolio</Link></li>
+            //             <li className="nav-item"><Link activeClass="active" to="section4" spy={true} smooth={true} duration={500}>Blog</Link></li>
+            //             <li className="nav-item"><Link activeClass="active" to="section5" spy={true} smooth={true} duration={500}>Contact</Link></li>
+            //             {/* </span> */}
+            //             {/* <span className="resume"> */}
+            //                 <li className="nav-item resume" onClick={this.handleClick}>Résumé</li>
+            //                 {/* </span> */}
+            //             <li className="nav-item bars">
+            //                 <i class="fas fa-bars fa-3x" onClick={this.makeResponsive}></i>
+            //             </li>
+            //         </ul>
+            //     </div>
+            // </nav>
         )
     }
 }
